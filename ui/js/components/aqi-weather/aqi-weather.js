453bd788fecadd4fe415282d7892eaa1cc9a113a
Jim Brighter <jbrighter92@gmail.com>
2022-12-14 14:34:13 -0500

    formatting

--- ui/js/components/aqi-weather/aqi-weather.js
+++ ui/js/components/aqi-weather/aqi-weather.js
@@ -3,6 +3,8 @@ import * as utils from '../../modules/utils.js';
 import { $ } from '../../modules/selectors.js';
 import { AQI_COLOR_MAP, AQI_DESC_MAP } from '../../modules/constants.js';
 
+const microgramsPerCubicMeter = 'μg/m<sup>3</sup>';
+
 const style = utils.createStyleElement(`
     @import "/js/components/aqi-weather/aqi-weather.css";
 `);
@@ -29,14 +31,14 @@ const define = (html) => {
             $('#aqi', this.shadowRoot).textContent = `${this.aqi ? this.aqi.main.aqi : 'error'} - ${AQI_DESC_MAP[this.aqi ? this.aqi.main.aqi : 5]}`;
             $('aqi-weather').classList.add(AQI_COLOR_MAP[this.aqi ? this.aqi.main.aqi : 5]);
 
-            $("#pollutant-co-val", this.shadowRoot).innerHTML = `${this.aqi.components.co} μg/m<sup>3</sup>`;
-            $("#pollutant-no-val", this.shadowRoot).innerHTML = `${this.aqi.components.no} μg/m<sup>3</sup>`;
-            $("#pollutant-no2-val", this.shadowRoot).innerHTML = `${this.aqi.components.no2} μg/m<sup>3</sup>`;
-            $("#pollutant-o3-val", this.shadowRoot).innerHTML = `${this.aqi.components.o3} μg/m<sup>3</sup>`;
-            $("#pollutant-so2-val", this.shadowRoot).innerHTML = `${this.aqi.components.so2} μg/m<sup>3</sup>`;
-            $("#pollutant-pm2_5-val", this.shadowRoot).innerHTML = `${this.aqi.components.pm2_5} μg/m<sup>3</sup>`;
-            $("#pollutant-pm10-val", this.shadowRoot).innerHTML = `${this.aqi.components.pm10} μg/m<sup>3</sup>`;
-            $("#pollutant-nh3-val", this.shadowRoot).innerHTML = `${this.aqi.components.nh3} μg/m<sup>3</sup>`;
+            $("#pollutant-co-val", this.shadowRoot).innerHTML = `${this.aqi.components.co} ${microgramsPerCubicMeter}`;
+            $("#pollutant-no-val", this.shadowRoot).innerHTML = `${this.aqi.components.no} ${microgramsPerCubicMeter}`;
+            $("#pollutant-no2-val", this.shadowRoot).innerHTML = `${this.aqi.components.no2} ${microgramsPerCubicMeter}`;
+            $("#pollutant-o3-val", this.shadowRoot).innerHTML = `${this.aqi.components.o3} ${microgramsPerCubicMeter}`;
+            $("#pollutant-so2-val", this.shadowRoot).innerHTML = `${this.aqi.components.so2} ${microgramsPerCubicMeter}`;
+            $("#pollutant-pm2_5-val", this.shadowRoot).innerHTML = `${this.aqi.components.pm2_5} ${microgramsPerCubicMeter}`;
+            $("#pollutant-pm10-val", this.shadowRoot).innerHTML = `${this.aqi.components.pm10} ${microgramsPerCubicMeter}`;
+            $("#pollutant-nh3-val", this.shadowRoot).innerHTML = `${this.aqi.components.nh3} ${microgramsPerCubicMeter}`;
         }
 
         get aqi() {
