2b1e74a07ba6b6eabd1bba175bbc36e9ca446064
Jim Brighter <jim-brighter@users.noreply.github.com>
2024-01-14 17:10:44 -0500

    Snow label (#116)
    * update label if there's snow
    
    * pull new nginx if necessary

--- local.sh
+++ local.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build -f Dockerfile -t jimsweather.xyz:latest .
+docker build --pull -f Dockerfile -t jimsweather.xyz:latest .
 docker run -d --rm --name jimsweather.xyz -p 9001:80 -v "$PWD/ui":/usr/share/nginx/html/ jimsweather.xyz:latest
