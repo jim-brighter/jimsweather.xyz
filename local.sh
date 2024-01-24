9298cf9c4cfc0c792bf4dd7d6f9c29115bd001ad
Jim Brighter <jbrighter92@gmail.com>
2024-01-24 00:26:15 -0500

    try to automatically open page

--- local.sh
+++ local.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 docker build --pull -f Dockerfile -t jimsweather.xyz:latest .
 docker run -d --rm --name jimsweather.xyz -p 9001:80 -v "$PWD/ui":/usr/share/nginx/html/ jimsweather.xyz:latest
+
+if [[ "$OSTYPE" =~ ^"darwin".*$ ]]; then
+    open "http://localhost:9001"
+else
+    echo "Link: http://localhost:9001"
+fi
