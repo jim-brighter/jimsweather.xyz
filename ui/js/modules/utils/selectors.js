3d8b33ac7c71be3a4f56c4ffc9af16c1a561d1ee
Jim Brighter <jbrighter92@gmail.com>
2024-07-15 16:16:59 -0400

    cleanup

--- ui/js/modules/utils/selectors.js
+++ ui/js/modules/utils/selectors.js
@@ -1,10 +1,8 @@
-const $ = (selector, node=null) => {
-    node = node || document;
+const $ = (selector, node = document) => {
     return node.querySelector(selector);
 }
 
-const $$ = (selector, node=null) => {
-    node = node || document;
+const $$ = (selector, node = document) => {
     return Array.prototype.slice.call(node.querySelectorAll(selector));
 }
 
