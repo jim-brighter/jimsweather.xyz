7efb0076196ff146e974fea1a1754c611054d146
Jim Brighter <jbrighter92@gmail.com>
2025-02-22 14:54:30 -0500

    deploy west - refactored

--- cdk/lib/cdk-stack-west.ts
+++ cdk/lib/cdk-stack-west.ts
@@ -6,20 +6,19 @@ import * as secrets from 'aws-cdk-lib/aws-secretsmanager'
 import * as nodelambda from 'aws-cdk-lib/aws-lambda-nodejs'
 import * as logs from 'aws-cdk-lib/aws-logs'
 import * as apigw from 'aws-cdk-lib/aws-apigateway'
-import { JimsHostedZoneStack } from './hosted-zone'
+import * as route53 from 'aws-cdk-lib/aws-route53'
+import * as targets from 'aws-cdk-lib/aws-route53-targets'
 
 export class JimsWeatherStackWest extends Stack {
 
-  readonly apiGateway : apigw.LambdaRestApi
-
-  constructor(scope: Construct, id: string, hostedZoneStack: JimsHostedZoneStack, props?: StackProps) {
+  constructor(scope: Construct, id: string, hostedZone: route53.IHostedZone, props?: StackProps) {
     super(scope, id, props)
 
     // SSL Certificate
     const cert = new certmanager.Certificate(this, 'JimsWeatherCertificate', {
       domainName: '*.jimsweather.xyz',
       subjectAlternativeNames: ['jimsweather.xyz'],
-      validation: certmanager.CertificateValidation.fromDns(hostedZoneStack.hostedZone)
+      validation: certmanager.CertificateValidation.fromDns(hostedZone)
     })
 
     // Default Error Lambda
@@ -51,7 +50,7 @@ export class JimsWeatherStackWest extends Stack {
     openweathermapSecret.grantRead(weatherLambda)
 
     // REST API
-    this.apiGateway = new apigw.LambdaRestApi(this, 'WeatherApi', {
+    const apiGateway = new apigw.LambdaRestApi(this, 'WeatherApi', {
       handler: defaultErrorLambda,
       proxy: false,
       domainName: {
@@ -61,12 +60,20 @@ export class JimsWeatherStackWest extends Stack {
       }
     })
 
-    const api = this.apiGateway.root.addResource('weather')
+    const api = apiGateway.root.addResource('weather')
     api.addCorsPreflight({
       allowOrigins: ['*'],
       allowHeaders: ['*'],
       allowCredentials: true
     })
     api.addMethod('GET', new apigw.LambdaIntegration(weatherLambda))
+
+    // Route53 API Record
+    new route53.ARecord(this, 'JimsWeatherApiRecordWest', {
+      zone: hostedZone,
+      recordName: 'api',
+      region: 'us-west-2',
+      target: route53.RecordTarget.fromAlias(new targets.ApiGateway(apiGateway))
+    })
   }
 }
